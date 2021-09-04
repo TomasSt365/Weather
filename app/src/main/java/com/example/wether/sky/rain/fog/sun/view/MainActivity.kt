26285fbccefa06823608d5b1c95485b53ff4ab67
Tomas.St <lapink540@gmail.com>
2021-09-04 21:24:37 +0300

    Создание MVC архетектуры приложения

--- app/src/main/java/com/example/wether/sky/rain/fog/sun/view/MainActivity.kt
+++ app/src/main/java/com/example/wether/sky/rain/fog/sun/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.wether.sky.rain.fog.sun
+package com.example.wether.sky.rain.fog.sun.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wether.sky.rain.fog.sun.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
