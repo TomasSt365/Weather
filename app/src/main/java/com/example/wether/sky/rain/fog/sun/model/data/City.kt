fb3909ccfa6e19084481f0a3f8d85d27268bdcd3
Tomas.St <lapink540@gmail.com>
2021-09-06 19:14:44 +0300

    Создание классов с данными

---
+++ app/src/main/java/com/example/wether/sky/rain/fog/sun/model/data/City.kt
@@ -0,0 +1,5 @@
+package com.example.wether.sky.rain.fog.sun.model.data
+
+class City {
+    var name: String? = null
+}
---
+++ app/src/main/java/com/example/wether/sky/rain/fog/sun/model/data/Weather.kt
@@ -0,0 +1,6 @@
+package com.example.wether.sky.rain.fog.sun.model.data
+
+class Weather {
+    var temperature: Int? = null
+    var feelsLike: Int? = null
+}
