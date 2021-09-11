37709062790c9b4be940dedf666152760426c7b0
Tomas.St <lapink540@gmail.com>
2021-09-11 23:07:51 +0300

    Оптимизирован Controller

--- app/src/main/java/com/example/wether/sky/rain/fog/sun/controller/Controller.kt
+++ app/src/main/java/com/example/wether/sky/rain/fog/sun/controller/Controller.kt
@@ -4,8 +4,5 @@ import com.example.wether.sky.rain.fog.sun.data.Weather
 import com.example.wether.sky.rain.fog.sun.data.City
 
 interface Controller {
-    fun getWeatherFromRemoteSource(): Weather
-    fun getWeatherFromLocalSource(): Weather
-
     fun getWeatherFromLocalStorage(): List<Weather>
 }
--- app/src/main/java/com/example/wether/sky/rain/fog/sun/controller/ControllerImpl.kt
+++ app/src/main/java/com/example/wether/sky/rain/fog/sun/controller/ControllerImpl.kt
@@ -5,14 +5,6 @@ import com.example.wether.sky.rain.fog.sun.data.getCites
 
 class ControllerImpl : Controller {
 
-    override fun getWeatherFromRemoteSource(): Weather {
-        return Weather()
-    }
-
-    override fun getWeatherFromLocalSource(): Weather {
-        return Weather()
-    }
-
     override fun getWeatherFromLocalStorage(): List<Weather> {
         return getCites()
     }
