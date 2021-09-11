3a80c057004f836607cb3674a15d0c04fa7097b1
Tomas.St <lapink540@gmail.com>
2021-09-11 22:25:20 +0300

    Создание и добавление отслеживания нажатий на элемент списка городов

--- app/src/main/java/com/example/wether/sky/rain/fog/sun/view/main/adapter/CityListAdaper.kt
+++ app/src/main/java/com/example/wether/sky/rain/fog/sun/view/main/adapter/CityListAdaper.kt
@@ -1,10 +1,11 @@
-package com.example.wether.sky.rain.fog.sun.view.main
+package com.example.wether.sky.rain.fog.sun.view.main.adapter
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wether.sky.rain.fog.sun.R
 import com.example.wether.sky.rain.fog.sun.data.Weather
@@ -13,7 +14,7 @@ class CityListAdapter : RecyclerView.Adapter<CityListAdapter.MainFragmentViewHol
 
     private var weatherData: List<Weather> = listOf()
     private var isDataSetRus: Boolean = true
-
+    private lateinit var listener: OnItemViewClickListener
 
     /*private var _biding: ItemListElementBinding? = null
     private val binding: ItemListElementBinding
@@ -25,6 +26,10 @@ class CityListAdapter : RecyclerView.Adapter<CityListAdapter.MainFragmentViewHol
         notifyDataSetChanged()
     }
 
+    fun setOnItemViewClickListener(onItemViewClickListener: OnItemViewClickListener) {
+        listener = onItemViewClickListener
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainFragmentViewHolder {
         return MainFragmentViewHolder(
             LayoutInflater.from(parent.context)
@@ -33,16 +38,35 @@ class CityListAdapter : RecyclerView.Adapter<CityListAdapter.MainFragmentViewHol
     }
 
     override fun onBindViewHolder(holder: MainFragmentViewHolder, position: Int) {
-        holder.render(weatherData[position])
+        holder.cityName.text = weatherData[position].city?.name ?: ""
     }
 
     override fun getItemCount() = weatherData.size
 
-    inner class MainFragmentViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        fun render(weather: Weather) {
-            itemView.findViewById<TextView>(R.id.cityName).text =
-                weather.city?.name ?: ""
+    inner class MainFragmentViewHolder(view: View) : RecyclerView.ViewHolder(view),
+        OnItemViewClickListener,
+        View.OnClickListener {
+        private val cardView: CardView = view.findViewById(R.id.itemContainer)
+        val cityName: TextView = view.findViewById(R.id.cityName)
+
+        override fun onItemClick(weather: Weather) {
+            listener.onItemClick(weather)
         }
+
+        override fun onClick(view: View?) {
+            if (view != null) {
+                when (view.id) {
+                    R.id.itemContainer -> {
+                        onItemClick(weather = weatherData[layoutPosition])//getPosition is Deprecated
+                    }
+                }
+            }
+        }
+
+        init {
+            cardView.setOnClickListener(this)
+        }
+
     }
 
     override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
---
+++ app/src/main/java/com/example/wether/sky/rain/fog/sun/view/main/adapter/OnItemViewClickListener.kt
@@ -0,0 +1,7 @@
+package com.example.wether.sky.rain.fog.sun.view.main.adapter
+
+import com.example.wether.sky.rain.fog.sun.data.Weather
+
+interface OnItemViewClickListener {
+    fun onItemClick(weather: Weather)
+}
